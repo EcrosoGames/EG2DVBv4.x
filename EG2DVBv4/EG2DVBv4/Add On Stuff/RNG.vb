9389968e055123e0cfc30230313bfac19d7c96a5
Brandon (SpaceMyName(9749)) Ecroso <greenbay123456789@gmail.com>
2017-02-10 00:09:29 -0800

    RNG fix
    Made it so that RNG actually works for procgen stuff.

--- EG2DVBv4/EG2DVBv4/Add On Stuff/RNG.vb
+++ EG2DVBv4/EG2DVBv4/Add On Stuff/RNG.vb
@@ -3,8 +3,9 @@
         Static Gen As System.Random = New System.Random()
         Return Gen.Next(Lo, Hi + 1)
     End Function
-    Public Shared Function Fixed(ByVal Lo As Integer, ByVal Hi As Integer, ByVal Seed As Integer) As Integer
-        Static Gen As System.Random = New System.Random(Seed)
+    Public Shared Gen As System.Random
+    Public Shared Function Fixed(ByVal Lo As Integer, ByVal Hi As Integer, ByVal Seed As Integer, Optional ByVal Reset As Boolean = False) As Integer
+        If Reset Then Gen = New System.Random(Seed)
         Return Gen.Next(Lo, Hi + 1)
     End Function
 End Class
