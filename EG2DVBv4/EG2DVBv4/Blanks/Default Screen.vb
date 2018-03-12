54d4c4f0f5ed01fd3b97154df3320b16f4fbe1a5
Brandon Ecroso <greenbay123456789@gmail.com>
2018-03-11 23:01:17 -0700

    More annoyances
    Removed "antitime" from Default Screen and title screen.

--- EG2DVBv4/EG2DVBv4/Blanks/Default Screen.vb
+++ EG2DVBv4/EG2DVBv4/Blanks/Default Screen.vb
@@ -1,7 +1,6 @@
 ﻿Public Class Default_Screen
     Inherits BaseScreen
     Private AniTime As Double = 0
-    Dim Antitime As Integer = 0
     Public Sub New()
         Name = "DefaultScreen"
         State = ScreenState.Active
@@ -14,9 +13,8 @@
         AniTime += Globals.GameTime.ElapsedGameTime.TotalMilliseconds
         If AniTime > 255 Then
             AniTime = 0
-            'Please use this so that when people turn off vsync the whole thing doesn't go to hell
+
         End If
-        Antitime = AniTime * 2
     End Sub
     Public Overrides Sub Draw()
         Globals.SpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone)
--- EG2DVBv4/EG2DVBv4/Screen Manager/Screens/TitleScreen.vb
+++ EG2DVBv4/EG2DVBv4/Screen Manager/Screens/TitleScreen.vb
@@ -1,16 +1,12 @@
 ﻿Public Class TitleScreen
     Inherits BaseScreen
     Private AniTime As Double = 0
-    Dim Antitime As Integer = 0
     Public Sub New()
         Name = "TitleScreen"
         State = ScreenState.Active
     End Sub
     Public Overrides Sub HandleInput()
         MyBase.HandleInput()
-        If Input.KeyPressed(Keys.P) Then
-            ScreenManager.BringToFront(Name)
-        End If
     End Sub
     Public Overrides Sub Update()
         'Example of how to control your updates
@@ -18,13 +14,12 @@
         If AniTime > 255 Then
             AniTime = 0
         End If
-        Antitime = AniTime * 2
     End Sub
     Public Overrides Sub Draw()
         MyBase.Draw()
         Globals.SpriteBatch.Begin(SpriteSortMode.Immediate, BlendState.AlphaBlend, SamplerState.PointClamp, DepthStencilState.Default, RasterizerState.CullNone)
         Globals.SpriteBatch.Draw(Textures.Null, New Rectangle(0, 0, Globals.GameSize.X, Globals.GameSize.Y), New Color(100, 0, 200))
-        Globals.SpriteBatch.DrawString(Fonts.MotorWerk_16, GameVersion.Name, New Vector2(Globals.GameSize.X / 2.05 - Fonts.MotorWerk_16.MeasureString(GameVersion.Name).X, Globals.GameSize.Y / 4), New Color(Antitime, 0, 0), 0, New Vector2(0, 0), 2.4, SpriteEffects.None, 0)
+        Globals.SpriteBatch.DrawString(Fonts.MotorWerk_16, GameVersion.Name, New Vector2(Globals.GameSize.X / 2.05 - Fonts.MotorWerk_16.MeasureString(GameVersion.Name).X, Globals.GameSize.Y / 4), New Color(CInt(AniTime), 0, 0), 0, New Vector2(0, 0), 2.4, SpriteEffects.None, 0)
         Globals.SpriteBatch.End()
     End Sub
     Public Overrides Sub remove()
