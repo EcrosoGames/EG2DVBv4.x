46b1485ef39dffacc12d39e9b28d8c8c600998e7
Brandon Ecroso <greenbay123456789@gmail.com>
2018-03-11 22:55:51 -0700

    Fixed annoyances
    Removed default permissions based on screen state upon creation. That
    was stupid as shit since that's not what permissions is meant for.

--- EG2DVBv4/EG2DVBv4/Screen Manager/ScreenManager.vb
+++ EG2DVBv4/EG2DVBv4/Screen Manager/ScreenManager.vb
@@ -1,23 +1,40 @@
 ﻿Public Enum ScreenState
     Active
-    ShutDown
-    Hidden
-    Special
+    Shutdown
+    Debug
+    Paused
+    Disabled
+    Privilaged
 End Enum
+'Leaving a ton of comments so that I don't get confused every time I go back to this.
 Public Class ScreenManager
-    Private Shared Screens As New List(Of BaseScreen)
+    Public Shared Screens As New List(Of BaseScreen)
     Private Shared NewScreens As New List(Of BaseScreen)
     Private DebugScreen As New Debug()
     Public Sub New()
-        AddScreen(DebugScreen)
+        AddScreen(DebugScreen) 'Starts up debug when game starts
     End Sub
     Public Sub Update()
         DebugScreen.Screens = "Screens: "
         Dim RemoveScreens As New List(Of BaseScreen)
         For Each FoundScreen As BaseScreen In Screens
-            If FoundScreen.State = ScreenState.ShutDown Then
+            If FoundScreen.State = ScreenState.Shutdown Then
                 RemoveScreens.Add(FoundScreen)
             Else
+                Select Case FoundScreen.State
+                    Case ScreenState.Active
+                        DebugScreen.Screens += "+"
+                    Case ScreenState.Shutdown 'Bug if can see this symbol/screen
+                        DebugScreen.Screens += "-"
+                    Case ScreenState.Debug
+                        DebugScreen.Screens += "~"
+                    Case ScreenState.Paused
+                        DebugScreen.Screens += "="
+                    Case ScreenState.Disabled
+                        DebugScreen.Screens += "#"
+                    Case ScreenState.Privilaged
+                        DebugScreen.Screens += "*"
+                End Select
                 DebugScreen.Screens += FoundScreen.Name + ", "
                 FoundScreen.Focused = False
             End If
@@ -29,7 +46,7 @@ Public Class ScreenManager
             Screens.Add(FoundScreen)
         Next
         NewScreens.Clear()
-        Screens.Remove(DebugScreen)
+        Screens.Remove(DebugScreen) 'Don't remove this, for some reason it's needed??????
         Screens.Add(DebugScreen)
         If Screens.Count > 0 Then
             For i = Screens.Count - 1 To 0 Step -1
@@ -41,78 +58,75 @@ Public Class ScreenManager
             Next
         End If
         For Each FoundScreen As BaseScreen In Screens
-            If Globals.WindowFocused And FoundScreen.State = ScreenState.Active Then
-                FoundScreen.HandleInput()
-            ElseIf Globals.WindowFocused And FoundScreen.State = ScreenState.Special Then
-                FoundScreen.HandleInput()
-            End If
-            If FoundScreen.State = ScreenState.Active Or FoundScreen.State = ScreenState.Special Then
-                FoundScreen.Update()
-            End If
+            Select Case FoundScreen.State
+                Case ScreenState.Active
+                    If Debug_Console.GameInput Then FoundScreen.HandleInput()
+                    FoundScreen.Update()
+                Case ScreenState.Debug
+                    If Debug_Console.GameInput Then FoundScreen.HandleInput()
+                    FoundScreen.Update()
+                Case ScreenState.Privilaged
+                    If Debug_Console.GameInput Then FoundScreen.HandleInput()
+                    FoundScreen.Update()
+            End Select
         Next
     End Sub
     Public Sub Draw()
         For Each FoundScreen As BaseScreen In Screens
-            If Not FoundScreen.Name = "Debug" And FoundScreen.State = ScreenState.Active Then
-                FoundScreen.Draw()
-            End If
-        Next
-        For Each FoundScreen As BaseScreen In Screens
-            If FoundScreen.Name = "Debug" And FoundScreen.State = ScreenState.Active Then
-                FoundScreen.Draw()
-            End If
+            Select Case FoundScreen.State
+                Case ScreenState.Active
+                    FoundScreen.Draw()
+                Case ScreenState.Debug
+                    If Globals.Debugging Then FoundScreen.Draw()
+                Case ScreenState.Paused
+                    FoundScreen.Draw()
+                Case ScreenState.Privilaged
+                    FoundScreen.Draw()
+            End Select
         Next
     End Sub
     Public Shared Sub AddScreen(screen As BaseScreen)
         NewScreens.Add(screen)
     End Sub
-    Public Shared Sub UnloadScreen(screen As String)
+    Public Shared Sub RemoveScreen(screen As String)
         For Each FoundScreen As BaseScreen In Screens
             If FoundScreen.Name = screen Then
-                FoundScreen.Unload()
+                FoundScreen.remove()
                 Exit For
             End If
         Next
     End Sub
-    Public Shared Sub PauseAll(Optional exempt As String = "HELP")
+    Public Shared Function Find(Optional Def As String = Nothing) As BaseScreen
         For Each FoundScreen As BaseScreen In Screens
-            If Not FoundScreen.Name = "Debug" And Globals.WindowFocused And FoundScreen.State = ScreenState.Active And FoundScreen.Name <> exempt Then
-                FoundScreen.State = ScreenState.Hidden
-            End If
-            FoundScreen.Update()
-        Next
-    End Sub
-    Public Shared Function Find(Optional Def As String = "Naming") As Boolean
-        For Each FoundScreen As BaseScreen In Screens
-            If FoundScreen.Name = Def Then
-                Return True
-            Else
-                Return False
-            End If
+            If Def = FoundScreen.Name Then Return FoundScreen
+            Exit For
         Next
     End Function
-    Public Shared Sub KillAll(Optional exempt As String = "Game Over")
-        For Each FoundScreen As BaseScreen In Screens
-            If Not FoundScreen.Name = "Debug" And Globals.WindowFocused And FoundScreen.State = ScreenState.Active And FoundScreen.Name <> exempt Then
-                FoundScreen.Unload()
-            End If
-            FoundScreen.Update()
-        Next
-    End Sub
-    Public Shared Sub ResumeAll()
-        If Screens.Count > 0 Then
-            For i = Screens.Count - 1 To 0 Step -1
-                If Screens(i).GrabFocus Then
-                    Screens(i).Focused = True
-                    Exit For
+    Public Shared Sub StateChange(Optional Screens() As BaseScreen = Nothing, Optional state As ScreenState = Nothing, Optional permission As Integer = -1)
+        If state = ScreenState.Shutdown Then
+            For Each FoundScreen As BaseScreen In Screens
+                If permission <> -1 Then
+                    If FoundScreen.Permission <= permission Then 'for doing all screens, but making sure important screens are not changed
+                        FoundScreen.remove()
+                    End If
+                Else
+                    FoundScreen.remove()
                 End If
             Next
+        Else
+            For Each FoundScreen As BaseScreen In Screens
+                For q As Integer = 0 To Screens.Length - 1
+                    If FoundScreen.Name = Screens(q).Name Then
+                        If permission <> -1 Then
+                            If FoundScreen.Permission <= permission Then 'for doing all screens, but making sure important screens are not changed
+                                FoundScreen.State = state
+                            End If
+                        Else
+                            FoundScreen.State = state ' changing individual screen states
+                        End If
+                    End If
+                Next
+            Next
         End If
-        For Each FoundScreen As BaseScreen In Screens
-            If Globals.WindowFocused And FoundScreen.State = ScreenState.Hidden Then
-                FoundScreen.State = ScreenState.Active
-            End If
-            FoundScreen.Update()
-        Next
     End Sub
 End Class
--- EG2DVBv4/EG2DVBv4/Screen Manager/Screens/TitleScreen.vb
+++ EG2DVBv4/EG2DVBv4/Screen Manager/Screens/TitleScreen.vb
@@ -8,6 +8,9 @@
     End Sub
     Public Overrides Sub HandleInput()
         MyBase.HandleInput()
+        If Input.KeyPressed(Keys.P) Then
+            ScreenManager.BringToFront(Name)
+        End If
     End Sub
     Public Overrides Sub Update()
         'Example of how to control your updates
@@ -24,7 +27,7 @@
         Globals.SpriteBatch.DrawString(Fonts.MotorWerk_16, GameVersion.Name, New Vector2(Globals.GameSize.X / 2.05 - Fonts.MotorWerk_16.MeasureString(GameVersion.Name).X, Globals.GameSize.Y / 4), New Color(Antitime, 0, 0), 0, New Vector2(0, 0), 2.4, SpriteEffects.None, 0)
         Globals.SpriteBatch.End()
     End Sub
-    Public Overrides Sub Unload()
-        MyBase.Unload()
+    Public Overrides Sub remove()
+        MyBase.remove()
     End Sub
 End Class
